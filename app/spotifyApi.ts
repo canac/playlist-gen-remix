47aae888efc9297d781730e7bfced0a6f1146660
Caleb Cox <canac@users.noreply.github.com>
2022-01-17 16:11:44 -0600

    Refresh access token when necessary

--- app/spotifyApi.ts
+++ app/spotifyApi.ts
@@ -4,6 +4,81 @@ import { PrismaClient, User } from '@prisma/client';
 import { chunk, differenceBy, map } from 'lodash';
 import { z } from 'zod';
 
+// Make a request to the Spotify API
+// This function takes care of adding the Spotify access token to the request
+// and retrying failures due to an expired access token
+// Return the response body JSON
+async function spotifyFetch(user: User, req: Request): Promise<unknown> {
+  let { accessToken } = user;
+
+  if (new Date() > user.accessTokenExpiresAt) {
+    // The access token is expired, so preemptively refresh it
+    console.log('Expired access token, retrying...');
+    accessToken = (await refreshAccessToken(user)).accessToken;
+  }
+
+  // Add the authorization headers to the provided request
+  const authorizedReq = new Request(req.url, req);
+  authorizedReq.headers.set('Authorization', `Bearer ${accessToken}`);
+  authorizedReq.headers.set('Accept', 'application/json');
+
+  console.log(`${req.method} ${req.url}`);
+  const res = await fetch(authorizedReq);
+  console.log(`Status: ${res.status}`);
+
+  const body = await res.json();
+  if (!res.ok) {
+    console.error('Spotify API error:');
+    console.error(body);
+    throw res;
+  }
+
+  return body;
+}
+
+// POST https://accounts.spotify.com/api/token
+// Only includes fields that we care about
+const TokenResponse = z.object({
+  access_token: z.string(),
+  expires_in: z.number(),
+});
+
+// Get the user a new Spotify access token
+// Return the User with the new access token
+async function refreshAccessToken(user: User): Promise<User> {
+  // Exchange the refresh token for an access token
+  const body = new URLSearchParams();
+  body.append('grant_type', 'refresh_token');
+  body.append('refresh_token', user.refreshToken);
+  const authorization = `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`;
+  const tokenRes = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body,
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Basic ${Buffer.from(authorization).toString('base64')}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+  const { access_token: accessToken, expires_in: expiresIn } =
+    TokenResponse.parse(await tokenRes.json());
+
+  // Save the new access token
+  const prisma = new PrismaClient();
+  const modifiedFields = {
+    accessToken,
+    // expiresIn is the length of the token's validity in seconds
+    // Calculate the absolute time when it will expire, considering it expired a minute
+    // sooner to avoid accidentally using an expired access token
+    accessTokenExpiresAt: new Date(Date.now() + (expiresIn - 60) * 1000),
+  };
+  await prisma.user.update({
+    where: { id: user.id },
+    data: modifiedFields,
+  });
+  return { ...user, ...modifiedFields };
+}
+
 // GET https://api.spotify.com/v1/me/tracks
 // Only includes fields that we care about
 const TracksResponse = z.object({
@@ -31,9 +106,7 @@ const TracksResponse = z.object({
 });
 
 // Pull the user's favorite tracks from Spotify into the database
-export async function syncFavoriteTracks(
-  user: Pick<User, 'id' | 'accessToken'>,
-): Promise<void> {
+export async function syncFavoriteTracks(user: User): Promise<void> {
   // At first, only load five tracks because the user is unlikely to have new favorites since the last time and we don't
   // want to transfer lots of new tracks unnecessarily
   let offset = 0;
@@ -41,16 +114,14 @@ export async function syncFavoriteTracks(
 
   while (true) {
     // Get the user's most recent favorite tracks from Spotify
-    const res = await fetch(
-      `https://api.spotify.com/v1/me/tracks?offset=${offset}&limit=${limit}`,
-      {
-        headers: {
-          Authorization: `Bearer ${user.accessToken}`,
-          Accept: 'application/json',
-        },
-      },
+    const tracks = TracksResponse.parse(
+      await spotifyFetch(
+        user,
+        new Request(
+          `https://api.spotify.com/v1/me/tracks?offset=${offset}&limit=${limit}`,
+        ),
+      ),
     );
-    const tracks = TracksResponse.parse(await res.json());
     const newTracks = tracks.items.map((item) => ({
       spotifyId: item.track.id,
       name: item.track.name,
@@ -103,23 +174,26 @@ export async function syncPlaylists(user: User): Promise<void> {
   });
   for (const label of newLabels) {
     // Create a new Spotify playlist for the label
-    const res = await fetch(
-      `https://api.spotify.com/v1/users/${user.spotifyId}/playlists`,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          name: `${label.name} [generated]`,
-          description: `Tracks labeled "${label.name}" by playlist-gen`,
-          public: false,
-        }),
-        headers: {
-          Authorization: `Bearer ${user.accessToken}`,
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-      },
+    const newPlaylist = {
+      name: `${label.name} [generated]`,
+      description: `Tracks labeled "${label.name}" by playlist-gen`,
+      public: false,
+    };
+    const { id: spotifyId } = CreatePlaylistResponse.parse(
+      await spotifyFetch(
+        user,
+        new Request(
+          `https://api.spotify.com/v1/users/${user.spotifyId}/playlists`,
+          {
+            method: 'POST',
+            body: JSON.stringify(newPlaylist),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          },
+        ),
+      ),
     );
-    const { id: spotifyId } = CreatePlaylistResponse.parse(await res.json());
 
     // Save the playlist to the database
     await prisma.playlist.create({
@@ -148,41 +222,39 @@ export async function syncPlaylists(user: User): Promise<void> {
     ).entries()) {
       const uris = spotifyIds.map((spotifyId) => `spotify:track:${spotifyId}`);
       console.log(uris);
-      const res = await fetch(
-        `https://api.spotify.com/v1/playlists/${
-          playlist.spotifyId
-        }/tracks?uris=${encodeURIComponent(uris.join(','))}`,
-        {
-          // During the first chunk, send PUT request to replace all previous tracks with the new chunk of tracks
-          // For subsequent chunks, send POST request to append the new chunk of tracks to the existing tracks
-          // to avoid overwriting the chunks that were just uploaded
-          method: index === 0 ? 'PUT' : 'POST',
-          headers: {
-            Authorization: `Bearer ${user.accessToken}`,
-            Accept: 'application/json',
+      await spotifyFetch(
+        user,
+        new Request(
+          `https://api.spotify.com/v1/playlists/${
+            playlist.spotifyId
+          }/tracks?uris=${encodeURIComponent(uris.join(','))}`,
+          {
+            // During the first chunk, send PUT request to replace all previous tracks with the new chunk of tracks
+            // For subsequent chunks, send POST request to append the new chunk of tracks to the existing tracks
+            // to avoid overwriting the chunks that were just uploaded
+            method: index === 0 ? 'PUT' : 'POST',
           },
-        },
+        ),
       );
-      await res.json();
     }
 
     // If the playlist needs to be emptied, remove the dummy track that we added to it
     if (playlist.label.tracks.length === 0) {
-      const res = await fetch(
-        `https://api.spotify.com/v1/playlists/${playlist.spotifyId}/tracks`,
-        {
-          method: 'DELETE',
-          body: JSON.stringify({
-            tracks: [{ uri: `spotify:track:${dummyTrackSpotifyId}` }],
-          }),
-          headers: {
-            Authorization: `Bearer ${user.accessToken}`,
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
+      const res = await spotifyFetch(
+        user,
+        new Request(
+          `https://api.spotify.com/v1/playlists/${playlist.spotifyId}/tracks`,
+          {
+            method: 'DELETE',
+            body: JSON.stringify({
+              tracks: [{ uri: `spotify:track:${dummyTrackSpotifyId}` }],
+            }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
           },
-        },
+        ),
       );
-      await res.json();
     }
   }
 }
