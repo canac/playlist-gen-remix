026a53c1859bb136de12ea73466b1778759895b7
Caleb Cox <canac@users.noreply.github.com>
2022-02-13 17:09:49 -0600

    Fix running out of database connections

--- app/lib/prisma.server.ts
+++ app/lib/prisma.server.ts
@@ -1,5 +1,25 @@
 import { PrismaClient } from '@prisma/client';
 
+declare global {
+  // eslint-disable-next-line no-var, vars-on-top
+  var prisma: PrismaClient;
+}
+
+let client: PrismaClient;
+if (process.env.NODE_ENV === 'production') {
+  client = new PrismaClient();
+} else {
+  // In development mode, store the prisma client on the global object instead
+  // of creating a new one every time the Remix dev server reloads, causing
+  // the database to run out of connections
+  // Reference: https://remix.run/docs/en/v1/other-api/serve
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+
+  client = global.prisma;
+}
+
 // Export a single prisma client instance used throughout the application to
 // avoid running out of database connections
-export const prisma = new PrismaClient();
+export const prisma = client;
