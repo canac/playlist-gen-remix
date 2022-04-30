f3858b8a3941cb389b08de6c86654c1cdfe7ba5d
Caleb Cox <canac@users.noreply.github.com>
2022-04-30 09:32:59 -0500

    Migrate to earley-bird

--- app/lib/labelGrammar.server.ts
+++ app/lib/labelGrammar.server.ts
@@ -1,36 +1,18 @@
-// Generated automatically by nearley, version 2.20.1
-// http://github.com/Hardmath123/nearley
-// Bypasses TS6133. Allow declared but unused functions.
-// @ts-ignore
-function id(d: any[]): any { return d[0]; }
-declare var addedKw: any;
-declare var comparison: any;
-declare var absoluteDate: any;
-declare var relativeDate: any;
-declare var releasedKw: any;
-declare var cleanKw: any;
-declare var explicitKw: any;
-declare var unlabeledKw: any;
-declare var labelKw: any;
-declare var labelId: any;
-declare var artistKw: any;
-declare var artistName: any;
-declare var lparen: any;
-declare var rparen: any;
-declare var not: any;
-declare var ws: any;
-declare var and: any;
-declare var or: any;
-
-import { Prisma } from '@prisma/client';
+// Generated automatically for earley-bird, version 0.0.3
+// http://github.com/canac/earley-bird
 import {
-  addDays,
-  addMonths,
-  addYears,
-  parse,
-  startOfDay,
-} from 'date-fns';
-import moo from 'moo';
+  Grammar,
+  Parser,
+  RuleSymbol,
+  CharsetSymbol,
+  LiteralSymbol,
+  TesterSymbol,
+  TokenSymbol,
+} from "@canac/earley-bird";
+
+import { Prisma } from "@prisma/client";
+import { addDays, addMonths, addYears, parse, startOfDay } from "date-fns";
+import moo from "moo";
 
 type DateTimeFilter = Prisma.DateTimeFilter;
 type TrackWhereInput = Prisma.TrackWhereInput;
@@ -40,30 +22,34 @@ const addFuncs = {
   m: addMonths,
   y: addYears,
 };
-type Unit = 'd' | 'm' | 'y';
+type Unit = "d" | "m" | "y";
 
 // Generate a comparison query between an absolute date in a given unit
 // makeAbsoluteComparison('=', new Date(2020, 1, 1), 'd') means "dates on the same day as 1/1/2020"
 // makeAbsoluteComparison('=', new Date(2020, 1, 1), 'y') means "dates in the same year as 1/1/2020"
 // makeAbsoluteComparison('>', new Date(2020, 1, 1), 'y') means "dates after 1/1/2020"
 // makeAbsoluteComparison('<=', new Date(2020, 1, 1), 'y') means "dates before or on 1/1/2020"
-function makeAbsoluteComparison(operator: string, date: Date, unit: Unit): DateTimeFilter {
+function makeAbsoluteComparison(
+  operator: string,
+  date: Date,
+  unit: Unit
+): DateTimeFilter {
   // Determine the next day/month/year based on the given unit
   const addFunc = addFuncs[unit];
   const next = addFunc(date, 1);
-  if (operator === '=') {
+  if (operator === "=") {
     // The model equals the date if it falls between the date and the next day/month/year
     return { gte: date, lt: next };
-  } else if (operator === '<') {
+  } else if (operator === "<") {
     return { lt: date };
-  } else if (operator === '<=') {
+  } else if (operator === "<=") {
     return { lt: next };
-  } else if (operator === '>') {
+  } else if (operator === ">") {
     return { gte: next };
-  } else if (operator === '>=') {
+  } else if (operator === ">=") {
     return { gte: date };
   } else {
-    throw new Error('Invalid operator');
+    throw new Error("Invalid operator");
   }
 }
 
@@ -72,32 +58,36 @@ function makeAbsoluteComparison(operator: string, date: Date, unit: Unit): DateT
 // makeRelativeComparison('=', 3, 'm') means "dates between 2 and 4 months before now"
 // makeRelativeComparison('>', 3, 'd') means "dates more than 3 days before now"
 // makeRelativeComparison('<=', 3, 'y') means "dates 3 or fewer years before now"
-function makeRelativeComparison(operator: string, amount: number, unit: Unit): DateTimeFilter {
+function makeRelativeComparison(
+  operator: string,
+  amount: number,
+  unit: Unit
+): DateTimeFilter {
   const addFunc = addFuncs[unit];
   const now = new Date();
-  if (operator === '=') {
+  if (operator === "=") {
     return { gt: addFunc(now, -amount - 1), lt: addFunc(now, -amount + 1) };
-  } else if (operator === '<') {
+  } else if (operator === "<") {
     return { gt: addFunc(now, -amount) };
-  } else if (operator === '<=') {
+  } else if (operator === "<=") {
     return { gte: addFunc(now, -amount) };
-  } else if (operator === '>') {
+  } else if (operator === ">") {
     return { lt: addFunc(now, -amount) };
-  } else if (operator === '>=') {
+  } else if (operator === ">=") {
     return { lte: addFunc(now, -amount) };
   } else {
-    throw new Error('Invalid operator');
+    throw new Error("Invalid operator");
   }
 }
 
 const lexer = moo.compile({
   ws: / +/,
 
-  cleanKw: 'clean',
-  explicitKw: 'explicit',
-  unlabeledKw: 'unlabeled',
-  addedKw: 'added',
-  releasedKw: 'released',
+  cleanKw: "clean",
+  explicitKw: "explicit",
+  unlabeledKw: "unlabeled",
+  addedKw: "added",
+  releasedKw: "released",
 
   relativeDate: {
     match: /[1-9]\d*[dmy]/,
@@ -111,81 +101,205 @@ const lexer = moo.compile({
     value: (v: string) =>
       v.length === 4
         ? {
-            unit: 'y',
-            date: parse(v, 'yyyy', startOfDay(new Date())),
+            unit: "y",
+            date: parse(v, "yyyy", startOfDay(new Date())),
           }
         : {
-            unit: 'd',
-            date: parse(v, 'M-d-yyyy', startOfDay(new Date())),
+            unit: "d",
+            date: parse(v, "M-d-yyyy", startOfDay(new Date())),
           },
   },
 
-  labelKw: 'label:',
+  labelKw: "label:",
   labelId: { match: /[1-9]\d*/, value: (v: string) => parseInt(v, 10) },
 
-  artistKw: 'artist:',
+  artistKw: "artist:",
   artistName: { match: /\".+?\"/, value: (v: string) => v.slice(1, -1) },
 
-  comparison: ['<=', '>=', '<', '>', '='],
-  not: '!',
-  and: '&&',
-  or: '||',
-  lparen: '(',
-  rparen: ')',
+  comparison: ["<=", ">=", "<", ">", "="],
+  not: "!",
+  and: "&&",
+  or: "||",
+  lparen: "(",
+  rparen: ")",
 });
 
-interface NearleyToken {
-  value: any;
-  [key: string]: any;
-};
-
-interface NearleyLexer {
-  reset: (chunk: string, info: any) => void;
-  next: () => NearleyToken | undefined;
-  save: () => any;
-  formatError: (token: never) => string;
-  has: (tokenType: string) => boolean;
-};
-
-interface NearleyRule {
-  name: string;
-  symbols: NearleySymbol[];
-  postprocess?: (d: any[], loc?: number, reject?: {}) => any;
-};
-
-type NearleySymbol = string | { literal: any } | { test: (token: any) => boolean };
-
-interface Grammar {
-  Lexer: NearleyLexer | undefined;
-  ParserRules: NearleyRule[];
-  ParserStart: string;
-};
-
-const grammar: Grammar = {
-  Lexer: lexer,
-  ParserRules: [
-    {"name": "main", "symbols": ["binary"], "postprocess": id},
-    {"name": "added", "symbols": [(lexer.has("addedKw") ? {type: "addedKw"} : addedKw), (lexer.has("comparison") ? {type: "comparison"} : comparison), (lexer.has("absoluteDate") ? {type: "absoluteDate"} : absoluteDate)], "postprocess": ([_, operator, date]): TrackWhereInput => ({ dateAdded: makeAbsoluteComparison(operator.value, date.value.date, date.value.unit) })},
-    {"name": "added", "symbols": [(lexer.has("addedKw") ? {type: "addedKw"} : addedKw), (lexer.has("comparison") ? {type: "comparison"} : comparison), (lexer.has("relativeDate") ? {type: "relativeDate"} : relativeDate)], "postprocess": ([_, operator, date]): TrackWhereInput => ({ dateAdded: makeRelativeComparison(operator.value, date.value.amount, date.value.unit)})},
-    {"name": "released", "symbols": [(lexer.has("releasedKw") ? {type: "releasedKw"} : releasedKw), (lexer.has("comparison") ? {type: "comparison"} : comparison), (lexer.has("absoluteDate") ? {type: "absoluteDate"} : absoluteDate)], "postprocess": ([_, operator, date]): TrackWhereInput => ({ dateReleased: makeAbsoluteComparison(operator.value, date.value.date, date.value.unit) })},
-    {"name": "released", "symbols": [(lexer.has("releasedKw") ? {type: "releasedKw"} : releasedKw), (lexer.has("comparison") ? {type: "comparison"} : comparison), (lexer.has("relativeDate") ? {type: "relativeDate"} : relativeDate)], "postprocess": ([_, operator, date]): TrackWhereInput => ({ dateReleased: makeRelativeComparison(operator.value, date.value.amount, date.value.unit)})},
-    {"name": "value", "symbols": [(lexer.has("cleanKw") ? {type: "cleanKw"} : cleanKw)], "postprocess": (_): TrackWhereInput => ({ explicit: false })},
-    {"name": "value", "symbols": [(lexer.has("explicitKw") ? {type: "explicitKw"} : explicitKw)], "postprocess": (_): TrackWhereInput => ({ explicit: true })},
-    {"name": "value", "symbols": [(lexer.has("unlabeledKw") ? {type: "unlabeledKw"} : unlabeledKw)], "postprocess": (_): TrackWhereInput => ({ labels: { none: {} } })},
-    {"name": "value", "symbols": [(lexer.has("labelKw") ? {type: "labelKw"} : labelKw), (lexer.has("labelId") ? {type: "labelId"} : labelId)], "postprocess": ([_, labelId]): TrackWhereInput => ({ labels: { some: { id: labelId.value } } })},
-    {"name": "value", "symbols": [(lexer.has("artistKw") ? {type: "artistKw"} : artistKw), (lexer.has("artistName") ? {type: "artistName"} : artistName)], "postprocess": ([_, artistName]): TrackWhereInput => ({ artist: { contains: artistName.value } })},
-    {"name": "value", "symbols": ["added"], "postprocess": id},
-    {"name": "value", "symbols": ["released"], "postprocess": id},
-    {"name": "parentheses", "symbols": [(lexer.has("lparen") ? {type: "lparen"} : lparen), "binary", (lexer.has("rparen") ? {type: "rparen"} : rparen)], "postprocess": ([_, inner]) => inner},
-    {"name": "parentheses", "symbols": ["value"], "postprocess": id},
-    {"name": "unary", "symbols": [(lexer.has("not") ? {type: "not"} : not), "parentheses"], "postprocess": ([ _, rhs ]: [unknown, TrackWhereInput]): TrackWhereInput => ({ NOT: rhs })},
-    {"name": "unary", "symbols": ["parentheses"], "postprocess": id},
-    {"name": "binary", "symbols": ["binary", (lexer.has("ws") ? {type: "ws"} : ws), (lexer.has("and") ? {type: "and"} : and), (lexer.has("ws") ? {type: "ws"} : ws), "unary"], "postprocess": ([lhs, _a, _b, _c, rhs]: [TrackWhereInput, unknown, unknown, unknown, TrackWhereInput]): TrackWhereInput => ({ AND: [lhs, rhs] })},
-    {"name": "binary", "symbols": ["binary", (lexer.has("ws") ? {type: "ws"} : ws), (lexer.has("or") ? {type: "or"} : or), (lexer.has("ws") ? {type: "ws"} : ws), "unary"], "postprocess": ([lhs, _a, _b, _c, rhs]: [TrackWhereInput, unknown, unknown, unknown, TrackWhereInput]): TrackWhereInput => ({ OR: [lhs, rhs] })},
-    {"name": "binary", "symbols": ["unary"], "postprocess": id},
-    {"name": "ws", "symbols": [(lexer.has("ws") ? {type: "ws"} : ws)]}
+const grammar = new Grammar(
+  [
+    {
+      name: "main",
+      symbols: [new RuleSymbol("binary")],
+      postprocess: (d) => d[0],
+    },
+    {
+      name: "added",
+      symbols: [
+        new TokenSymbol("addedKw"),
+        new TokenSymbol("comparison"),
+        new TokenSymbol("absoluteDate"),
+      ],
+      postprocess: ([_, operator, date]): TrackWhereInput => ({
+        dateAdded: makeAbsoluteComparison(
+          operator.value,
+          date.value.date,
+          date.value.unit
+        ),
+      }),
+    },
+    {
+      name: "added",
+      symbols: [
+        new TokenSymbol("addedKw"),
+        new TokenSymbol("comparison"),
+        new TokenSymbol("relativeDate"),
+      ],
+      postprocess: ([_, operator, date]): TrackWhereInput => ({
+        dateAdded: makeRelativeComparison(
+          operator.value,
+          date.value.amount,
+          date.value.unit
+        ),
+      }),
+    },
+    {
+      name: "released",
+      symbols: [
+        new TokenSymbol("releasedKw"),
+        new TokenSymbol("comparison"),
+        new TokenSymbol("absoluteDate"),
+      ],
+      postprocess: ([_, operator, date]): TrackWhereInput => ({
+        dateReleased: makeAbsoluteComparison(
+          operator.value,
+          date.value.date,
+          date.value.unit
+        ),
+      }),
+    },
+    {
+      name: "released",
+      symbols: [
+        new TokenSymbol("releasedKw"),
+        new TokenSymbol("comparison"),
+        new TokenSymbol("relativeDate"),
+      ],
+      postprocess: ([_, operator, date]): TrackWhereInput => ({
+        dateReleased: makeRelativeComparison(
+          operator.value,
+          date.value.amount,
+          date.value.unit
+        ),
+      }),
+    },
+    {
+      name: "value",
+      symbols: [new TokenSymbol("cleanKw")],
+      postprocess: (_): TrackWhereInput => ({ explicit: false }),
+    },
+    {
+      name: "value",
+      symbols: [new TokenSymbol("explicitKw")],
+      postprocess: (_): TrackWhereInput => ({ explicit: true }),
+    },
+    {
+      name: "value",
+      symbols: [new TokenSymbol("unlabeledKw")],
+      postprocess: (_): TrackWhereInput => ({ labels: { none: {} } }),
+    },
+    {
+      name: "value",
+      symbols: [new TokenSymbol("labelKw"), new TokenSymbol("labelId")],
+      postprocess: ([_, labelId]): TrackWhereInput => ({
+        labels: { some: { id: labelId.value } },
+      }),
+    },
+    {
+      name: "value",
+      symbols: [new TokenSymbol("artistKw"), new TokenSymbol("artistName")],
+      postprocess: ([_, artistName]): TrackWhereInput => ({
+        artist: { contains: artistName.value },
+      }),
+    },
+    {
+      name: "value",
+      symbols: [new RuleSymbol("added")],
+      postprocess: (d) => d[0],
+    },
+    {
+      name: "value",
+      symbols: [new RuleSymbol("released")],
+      postprocess: (d) => d[0],
+    },
+    {
+      name: "parentheses",
+      symbols: [
+        new TokenSymbol("lparen"),
+        new RuleSymbol("binary"),
+        new TokenSymbol("rparen"),
+      ],
+      postprocess: ([_, inner]) => inner,
+    },
+    {
+      name: "parentheses",
+      symbols: [new RuleSymbol("value")],
+      postprocess: (d) => d[0],
+    },
+    {
+      name: "unary",
+      symbols: [new TokenSymbol("not"), new RuleSymbol("parentheses")],
+      postprocess: ([_, rhs]: [unknown, TrackWhereInput]): TrackWhereInput => ({
+        NOT: rhs,
+      }),
+    },
+    {
+      name: "unary",
+      symbols: [new RuleSymbol("parentheses")],
+      postprocess: (d) => d[0],
+    },
+    {
+      name: "binary",
+      symbols: [
+        new RuleSymbol("binary"),
+        new TokenSymbol("ws"),
+        new TokenSymbol("and"),
+        new TokenSymbol("ws"),
+        new RuleSymbol("unary"),
+      ],
+      postprocess: ([lhs, _a, _b, _c, rhs]: [
+        TrackWhereInput,
+        unknown,
+        unknown,
+        unknown,
+        TrackWhereInput
+      ]): TrackWhereInput => ({ AND: [lhs, rhs] }),
+    },
+    {
+      name: "binary",
+      symbols: [
+        new RuleSymbol("binary"),
+        new TokenSymbol("ws"),
+        new TokenSymbol("or"),
+        new TokenSymbol("ws"),
+        new RuleSymbol("unary"),
+      ],
+      postprocess: ([lhs, _a, _b, _c, rhs]: [
+        TrackWhereInput,
+        unknown,
+        unknown,
+        unknown,
+        TrackWhereInput
+      ]): TrackWhereInput => ({ OR: [lhs, rhs] }),
+    },
+    {
+      name: "binary",
+      symbols: [new RuleSymbol("unary")],
+      postprocess: (d) => d[0],
+    },
+    { name: "ws", symbols: [new TokenSymbol("ws")], postprocess: undefined },
   ],
-  ParserStart: "main",
-};
+  "main"
+);
 
-export default grammar;
+const parser = new Parser(grammar, lexer);
+
+export default parser;
--- app/lib/smartLabel.server.ts
+++ app/lib/smartLabel.server.ts
@@ -1,27 +1,15 @@
 import { Prisma } from '@prisma/client';
-import { Grammar, Parser } from 'nearley';
-import grammar from '~/lib/labelGrammar.server';
-
-// Initialize the parser for the smart label criteria
-const parser = new Parser(Grammar.fromCompiled(grammar));
+import parser from '~/lib/labelGrammar.server';
 
 // Generate a prisma `where` clause from a criteria string or null if the criteria is invalid
 export function generatePrismaFilter(
   criteria: string,
 ): Prisma.TrackWhereInput | null {
-  try {
-    const state = parser.save();
-    parser.feed(criteria);
-    const filter = parser.results[0] as Prisma.TrackWhereInput;
-    // Save and restore the parser state so that we can reuse the parser instance
-    parser.restore(state);
-    return filter;
-  } catch (err) {
-    return null;
-  }
+  const result = parser.parse(criteria);
+  return result.success ? (result.result as Prisma.TrackWhereInput) : null;
 }
 
 // Return a boolean indicating whether the smart criteria is valid
 export function validateSmartCriteria(criteria: string): boolean {
-  return generatePrismaFilter(criteria) !== null;
+  return parser.parse(criteria).success;
 }
